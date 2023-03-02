07f5fe8e6b47c345de1542ba3b829b6147d2dbe7
arishorts <arieljschwartz@gmail.com>
2023-03-01 20:58:18 -0700

    fixed clog middleware bug that left the command prompt in cyan text. Also added process_child function to automatically open the browser on server spin up

--- middleware/clog.js
+++ middleware/clog.js
@@ -1,17 +1,22 @@
 // Custom middleware that logs out the type and path of each request to the server
 const clog = (req, res, next) => {
-  const fgCyan = '\x1b[36m';
+  const fgCyan = "\x1b[36m";
+  const ANSIoff = "\x1b[0m";
   switch (req.method) {
-    case 'GET': {
-      console.info(`📗 ${fgCyan}${req.method} request to ${req.path}`);
+    case "GET": {
+      console.info(
+        `📗 ${fgCyan}${req.method} request to ${req.path}${ANSIoff}`
+      );
       break;
     }
-    case 'POST': {
-      console.info(`📘 ${fgCyan}${req.method} request to ${req.path}`);
+    case "POST": {
+      console.info(
+        `📘 ${fgCyan}${req.method} request to ${req.path}${ANSIoff}`
+      );
       break;
     }
     default:
-      console.log(`📙${fgCyan}${req.method} request to ${req.path}`);
+      console.log(`📙${fgCyan}${req.method} request to ${req.path}${ANSIoff}`);
   }
 
   next();
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { clog } = require("./middleware/clog");
 const path = require("path");
 const api = require("./routes/index.js");
 const app = express();
+const { exec } = require("child_process");
+const os = require("os");
 
 // Import custom middleware, "clog"
 app.use(clog);
@@ -32,6 +34,20 @@ app.get("/", (req, res) =>
 
 //listening on port ______
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port} 🚀`)
-);
+// app.listen(port, () =>
+//   console.log(`App listening at http://localhost:${port} 🚀`)
+// );
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port} 🚀`);
+
+  if (os.type() === "Darwin") {
+    // Check if the OS is macOS
+    exec(`open http://localhost:${port}`);
+  } else if (os.type() === "Windows_NT") {
+    // Check if the OS is Windows
+    exec(`start http://localhost:${port}`);
+  } else {
+    console.log("Unsupported OS detected");
+  }
+});
