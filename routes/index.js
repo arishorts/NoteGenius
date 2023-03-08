84b8e704182ed80aa9324c9eedfbfb690c80e903
arishorts <arieljschwartz@gmail.com>
2023-03-08 15:25:27 -0700

    npm start can now be used to start the app

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,10 @@ const express = require("express");
 const { clog } = require("../middleware/clog");
 
 // Import our modular routers for /tips and /feedback
-//onst tipsRouter = require("./tips");
-//const feedbackRouter = require("./feedback");
 const notesRouter = require("./notes");
 
 const app = express();
 
-//app.use("/tips", tipsRouter);
-//app.use("/feedback", feedbackRouter);
 app.use("/notes", notesRouter);
 
 // Initialize custom middleware
